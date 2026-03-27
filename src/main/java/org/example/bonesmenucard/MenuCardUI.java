c27042423ee461804daefc1d5dd14f2f01199bb6
Mikkelno1 <mikkelnorinder@live.dk>
2026-03-27 10:29:36 +0100

    Created project

---
+++ src/main/java/org/example/bonesmenucard/MenuCardUI.java
@@ -0,0 +1,40 @@
+package org.example.bonesmenucard;
+
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.Pane;
+
+public class MenuCardUI
+{
+    ScrollPane sPane = new ScrollPane();
+
+    RadioButton rb1 = new RadioButton();
+    RadioButton rb2 = new RadioButton();
+    RadioButton rb3 = new RadioButton();
+    RadioButton rb4 = new RadioButton();
+    RadioButton rb5 = new RadioButton();
+    RadioButton rb6 = new RadioButton();
+    RadioButton rb7 = new RadioButton();
+    RadioButton rb8 = new RadioButton();
+    RadioButton rb9 = new RadioButton();
+    RadioButton rb10 = new RadioButton();
+    RadioButton rb11 = new RadioButton();
+    RadioButton rb12 = new RadioButton();
+    RadioButton rb13 = new RadioButton();
+    RadioButton rb14 = new RadioButton();
+    RadioButton rb15 = new RadioButton();
+    RadioButton rb16 = new RadioButton();
+    RadioButton rb17 = new RadioButton();
+    RadioButton rb18 = new RadioButton();
+    RadioButton rb19 = new RadioButton();
+    RadioButton rb20 = new RadioButton();
+    RadioButton rb21 = new RadioButton();
+
+
+
+
+
+
+
+
+}
