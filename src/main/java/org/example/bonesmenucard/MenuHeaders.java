b7e1a4533b970a3bd81de1fa59d025dd72cacc7c
Mads Krabbe <spiligen1@gmail.com>
2026-03-27 11:53:05 +0100

    defining header

--- src/main/java/org/example/bonesmenucard/MenuHeaders.java
+++ src/main/java/org/example/bonesmenucard/MenuHeaders.java
@@ -1,8 +1,6 @@
 package org.example.bonesmenucard;
 
 import javafx.scene.control.Label;
-import javafx.scene.text.Font;
-import com.sun.javafx.css.StyleManager;
 
 public class MenuHeaders extends Label
 {
